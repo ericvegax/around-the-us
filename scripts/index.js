bf906fbeb028e5cc70839e01cf62dafd3010901c
Eric V. <ericvegax@users.noreply.github.com>
2024-08-13 21:01:27 -0400

    project_4_stage_3

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,95 @@
-const initialCards = 
-[
-    {
+document.addEventListener("DOMContentLoaded", () => {
+    const initializeCards = [
+      {
         name: "Yosemite Valley",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
-    },
-    {
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+      },
+      {
         name: "Lake Louise",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
-    },
-    {
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+      },
+      {
         name: "Bald Mountains",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
-    },
-    {
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+      },
+      {
         name: "Latemar",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
-    },
-    {
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+      },
+      {
         name: "Vanoise National Park",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
-    },
-    {
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+      },
+      {
         name: "Lago di Braies",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
+        link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+      },
+    ];
+  
+    const profileEditButton = document.querySelector(".profile__edit-button");
+    const profileEditModal = document.querySelector("#profile-edit-modal");
+    const profileCloseButton = document.querySelector("#modal-close-button");
+    const profileTitle = document.querySelector(".profile__title");
+    const profileDescription = document.querySelector(".profile__descr");
+    const profileTitleInput = document.querySelector("#profile-title-input");
+    const profileDescriptionInput = document.querySelector("#profile-descr-input");
+    const profileEditForm = document.querySelector("#profile-edit-form");
+  
+    const elementListElement = document.querySelector(".cards__list");
+    const elementTemplate = document.querySelector("#element-template").content.firstElementChild;
+  
+    function closePopUp(popUp) {
+      popUp.classList.remove("modal_opened");
     }
-];
+  
+    function openPopUp(popUp) {
+      popUp.classList.add("modal_opened");
+    }
+  
+    function handleProfileEditSubmit(e) {
+      e.preventDefault();
+      profileTitle.textContent = profileTitleInput.value;
+      profileDescription.textContent = profileDescriptionInput.value;
+      closePopUp(profileEditModal);
+    }
+  
+    function renderElement(element, container) {
+      container.prepend(element);
+    }
+  
+    function getElementView(elementData) {
+      const element = elementTemplate.cloneNode(true);
+      const elementImageElement = element.querySelector(".element__img");
+      const elementTitleElement = element.querySelector(".element__text");
+  
+      elementImageElement.src = elementData.link;
+      elementImageElement.alt = elementData.name;
+      elementTitleElement.textContent = elementData.name;
+      return element;
+    }
+  
+    if (profileEditButton) {
+      profileEditButton.addEventListener("click", () => {
+        profileTitleInput.value = profileTitle.textContent;
+        profileDescriptionInput.value = profileDescription.textContent;
+        openPopUp(profileEditModal);
+      });
+    }
+  
+    if (profileCloseButton) {
+      profileCloseButton.addEventListener("click", () => {
+        closePopUp(profileEditModal);
+      });
+    }
+  
+    if (profileEditForm) {
+      profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+    }
+  
+    if (elementListElement && elementTemplate) {
+      initializeCards.forEach((elementData) => {
+        const elementView = getElementView(elementData);
+        renderElement(elementView, elementListElement);
+      });
+    }
+  });
